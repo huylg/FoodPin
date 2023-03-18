c42a3a63aa546f157fd74d2fb5d6fa3c4e8ec754
huy <hlg@elca.vn>
2023-03-18 16:16:10 +0700

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Ly Gia Huy on 18/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
